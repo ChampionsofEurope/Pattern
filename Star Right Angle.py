bf1fc60ec462caf09761c5968983aae60f4098ca
ChampionsofEurope <86567033+ChampionsofEurope@users.noreply.github.com>
2021-08-29 18:12:43 +0100

    Create Star Right Angle.py

---
+++ Star Right Angle.py
@@ -0,0 +1,12 @@
+n = int(input("Enter the number of rows:"))
+m = 2 * n - 2
+for i in range(0, n):
+    for j in range(0, m):
+        print(end=" ")
+
+    m = m - 2
+
+    for j in range(0, i + 1):
+        print("* ", end="")
+
+    print("")
